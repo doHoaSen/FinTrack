16a01d46cca43f5986f18da01616788e6226bdc7
doHoaSen <applesusu999@gmail.com>
2025-12-10 18:42:14 +0900

    251203 벡엔드 - 지출 피드백 개선 위해 FeedbackResponse 수정 및 ExpenseFeedbackService 수정 - UX (문구 등) 개선

--- FinTrack_BE/src/main/java/doHoaSen/FinTrack/expenseFeedback/dto/FeedbackResponse.java
+++ FinTrack_BE/src/main/java/doHoaSen/FinTrack/expenseFeedback/dto/FeedbackResponse.java
@@ -1,24 +1,98 @@
 package doHoaSen.FinTrack.expenseFeedback.dto;
 
-import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-@Getter @Setter
-
+@Getter
+@Builder
 public class FeedbackResponse {
-    private String monthlyTrend;
-    private String weekdayPattern;
-    private String hourlyPattern;
-    private String categoryPattern;
-    private String targetProgress;
 
-    private String fixedVsVariable;
-    private String categoryConcentration;
-    private String dailyAverageTrend;
-    private String spikeDetection;
-    private String overSpendSequence;
+    private MonthlyTrend monthlyTrend;
+    private WeekdayPattern weekdayPattern;
+    private HourlyPattern hourlyPattern;
+    private CategoryPattern categoryPattern;
+    private TargetProgress targetProgress;
 
+    private FixedVsVariable fixedVsVariable;
+    private CategoryConcentration categoryConcentration;
+    private DailyAverageTrend dailyAverageTrend;
+    private SpikeDetection spikeDetection;
+    private OverSpendSequence overSpendSequence;
 
+    @Getter @Builder
+    public static class MonthlyTrend {
+        private String status;
+        private Long currentMonth;
+        private Long lastMonth;
+        private Double percentDiff;
+        private String message;
+    }
+
+    @Getter @Builder
+    public static class WeekdayPattern {
+        private Integer peakDay;
+        private Long peakAmount;
+        private String message;
+    }
+
+    @Getter @Builder
+    public static class HourlyPattern {
+        private Integer peakHour;
+        private Long amount;
+        private String message;
+    }
+
+    @Getter @Builder
+    public static class CategoryPattern {
+        private String topCategory;
+        private Long amount;
+        private Double ratio;
+        private String message;
+    }
+
+    @Getter @Builder
+    public static class TargetProgress {
+        private Long targetAmount;
+        private Long usedAmount;
+        private Double ratio;
+        private String message;
+    }
+
+    @Getter @Builder
+    public static class FixedVsVariable {
+        private Long fixedTotal;
+        private Long variableTotal;
+        private Double fixedRatio;
+        private Double variableRatio;
+        private String message;
+    }
+
+    @Getter @Builder
+    public static class CategoryConcentration {
+        private String topCategory;
+        private Long amount;
+        private Double ratio;
+        private String message;
+    }
+
+    @Getter @Builder
+    public static class DailyAverageTrend {
+        private Double currentAvg;
+        private Double lastMonthAvg;
+        private Double diff;
+        private String message;
+    }
+
+    @Getter @Builder
+    public static class SpikeDetection {
+        private String date;
+        private Long amount;
+        private String message;
+    }
+
+    @Getter @Builder
+    public static class OverSpendSequence {
+        private Integer streak;
+        private String message;
+    }
 }
