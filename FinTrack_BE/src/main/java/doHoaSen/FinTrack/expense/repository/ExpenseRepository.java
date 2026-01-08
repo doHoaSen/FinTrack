8ac301d6d7112d1f56751087e8fc6366e44b41af
doHoaSen <applesusu999@gmail.com>
2026-01-08 16:58:18 +0900

    260108 백엔드 - 최근 지출 내역을 대시보드 페이지에 보이도록 코드 추가

--- FinTrack_BE/src/main/java/doHoaSen/FinTrack/expense/repository/ExpenseRepository.java
+++ FinTrack_BE/src/main/java/doHoaSen/FinTrack/expense/repository/ExpenseRepository.java
@@ -3,6 +3,7 @@ package doHoaSen.FinTrack.expense.repository;
 import doHoaSen.FinTrack.expense.entity.Expense;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import org.springframework.data.domain.Pageable;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -14,4 +15,8 @@ public interface ExpenseRepository extends JpaRepository<Expense, Long> {
             LocalDateTime start,
             LocalDateTime end
     );
+
+    List<Expense> findByUserIdOrderByExpenseAtDesc(Long userId, Pageable pageable);
+
+    List<Expense> findTop10ByUserIdOrderByExpenseAtDesc(Long userId);
 }
